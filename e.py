d05666dd185729a3d7bd390d9bd44afe52c1795e
dariahnn <dariananandaz@gmail.com>
2025-02-17 15:12:18 +0300

    palindrome

---
+++ e.py
@@ -0,0 +1,8 @@
+def palindrome(word):
+    word = word.lower()
+    return word == word[::-1]
+word = input("Enter a word: ")
+if palindrome(word):
+    print(f"{word} is a palindrome")
+else:
+    print(f"{word} is not a palindrome")
